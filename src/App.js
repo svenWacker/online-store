9c011f0ba1c477f451348df44b8069acb80522f0
svenWacker <s.u.wacker@outlook.de>
2021-06-14 14:17:46 +0200

    importig packages

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@ import React, { useState } from "react";
 import Data from "./data.json";
 import ProductList from "./components/ProductList";
 import Footer from "./components/Footer";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-// import { library } from "@fortawesome/fontawesome-svg-core";
-// import { fab } from "@fortawesome/free-brands-svg-icons";
-// import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
-import { FaBeer } from "react-icons/fa";
-import { WiAlien, WiNightAltThunderstorm } from "react-icons/wi";
-
+// import { WiAlien, WiNightAltThunderstorm } from "react-icons/wi";
+// import { BiCartAlt } from "react-icons/bi";
+import { FaMoneyBillAlt, FaShoppingCart } from "react-icons/fa";
 //library.add(fab, faCheckSquare, faCoffee);
+
 const App = () => {
   const [data, setData] = useState(Data);
   const [userInput, setUserInput] = useState("");
@@ -42,8 +39,7 @@ const App = () => {
 
       <h1>Welcome to our online store</h1>
       <h3>
-        Lets go for a <FaBeer /> with <WiAlien /> when it's
-        <WiNightAltThunderstorm />
+        Lets spent some <FaMoneyBillAlt /> and fill the <FaShoppingCart />
       </h3>
       <form onSubmit={handleSubmit}>
         <input type="text" onChange={changeHandle} value={userInput} />
